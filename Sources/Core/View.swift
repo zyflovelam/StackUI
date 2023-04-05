4d7c8318c52a385c655124df6aa685d44109fc28
Zyf <zyflovelam@hotmail.com>
2023-04-05 16:50:49 +0800

    Update View.swift

--- Sources/Core/View.swift
+++ Sources/Core/View.swift
@@ -9,6 +9,8 @@ import UIKit
 
 public protocol StackUIView: StackUILayer {
     func size(width: CGFloat?, height: CGFloat?) -> Self
+    func width(_ width: CGFloat) -> Self
+    func height(_ height: CGFloat) -> Self
     func isUserInteractionEnabled(_ isUserInteractionEnabled: Bool) -> Self
     func tag(_ tag: Int) -> Self
     @available(iOS 14.0, *)
@@ -40,6 +42,7 @@ public protocol StackUIView: StackUILayer {
     func contentHuggingPriority(_ priority: UILayoutPriority, for axis: NSLayoutConstraint.Axis) -> Self
     func contentCompressionResistancePriority(_ priority: UILayoutPriority, for axis: NSLayoutConstraint.Axis) -> Self
 }
+
 public extension StackUIView {
     func size(width: CGFloat? = nil, height: CGFloat? = nil) -> Self {
         translatesAutoresizingMaskIntoConstraints = false
@@ -51,81 +54,110 @@ public extension StackUIView {
         }
         return self
     }
+
+    func width(_ width: CGFloat) -> Self {
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+        return self
+    }
+    
+    func height(_ height: CGFloat) -> Self {
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+        return self
+    }
+
     func isUserInteractionEnabled(_ isUserInteractionEnabled: Bool) -> Self {
         self.isUserInteractionEnabled = isUserInteractionEnabled
         return self
     }
+
     func tag(_ tag: Int) -> Self {
         self.tag = tag
         return self
     }
+
     @available(iOS 14.0, *)
     func focusGroupIdentifier(_ focusGroupIdentifier: String?) -> Self {
         self.focusGroupIdentifier = focusGroupIdentifier
         return self
     }
+
     @available(iOS 15.0, *)
     func focusGroupPriority(_ focusGroupPriority: UIFocusGroupPriority) -> Self {
         self.focusGroupPriority = focusGroupPriority
         return self
     }
+
     @available(iOS 15.0, *)
     func focusEffect(_ focusEffect: UIFocusEffect?) -> Self {
         self.focusEffect = focusEffect
         return self
     }
+
     func semanticContentAttribute(_ semanticContentAttribute: UISemanticContentAttribute) -> Self {
         self.semanticContentAttribute = semanticContentAttribute
         return self
     }
+
     func transform(_ transform: CGAffineTransform) -> Self {
         self.transform = transform
         return self
     }
+
     @available(iOS 13.0, *)
     func transform3D(_ transform3D: CATransform3D) -> Self {
         self.transform3D = transform3D
         return self
     }
+
     func contentScaleFactor(_ contentScaleFactor: CGFloat) -> Self {
         self.contentScaleFactor = contentScaleFactor
         return self
     }
+
     func isMultipleTouchEnabled(_ isMultipleTouchEnabled: Bool) -> Self {
         self.isMultipleTouchEnabled = isMultipleTouchEnabled
         return self
     }
+
     func isExclusiveTouch(_ isExclusiveTouch: Bool) -> Self {
         self.isExclusiveTouch = isExclusiveTouch
         return self
     }
+
     func clipsToBounds(_ clipsToBounds: Bool) -> Self {
         self.clipsToBounds = clipsToBounds
         return self
     }
+
     func backgroundColor(_ backgroundColor: UIColor?) -> Self {
         self.backgroundColor = backgroundColor
         return self
     }
+
     func alpha(_ alpha: CGFloat) -> Self {
         self.alpha = alpha
         return self
     }
+
     func isOpaque(_ isOpaque: Bool) -> Self {
         self.isOpaque = isOpaque
         return self
     }
+
     func clearsContextBeforeDrawing(_ clearsContextBeforeDrawing: Bool) -> Self {
         self.clearsContextBeforeDrawing = clearsContextBeforeDrawing
         return self
     }
+
     func isHidden(_ isHidden: Bool) -> Self {
         self.isHidden = isHidden
         return self
     }
 
     func isHidden(_ publisher: Publisher<Bool>) -> Self {
-        publisher.addSubscriber {[weak self] isHidden in
+        publisher.addSubscriber { [weak self] isHidden in
             self?.isHidden = isHidden
         }
         return self
@@ -135,39 +167,46 @@ public extension StackUIView {
         self.contentMode = contentMode
         return self
     }
+
     func mask(_ mask: UIView?) -> Self {
         self.mask = mask
         return self
     }
+
     func tintColor(_ tintColor: UIColor!) -> Self {
         self.tintColor = tintColor
         return self
     }
+
     func tintAdjustmentMode(_ tintAdjustmentMode: UIView.TintAdjustmentMode) -> Self {
         self.tintAdjustmentMode = tintAdjustmentMode
         return self
     }
+
     func restorationIdentifier(_ restorationIdentifier: String?) -> Self {
         self.restorationIdentifier = restorationIdentifier
         return self
     }
+
     @available(iOS 13.0, *)
     func overrideUserInterfaceStyle(_ overrideUserInterfaceStyle: UIUserInterfaceStyle) -> Self {
         self.overrideUserInterfaceStyle = overrideUserInterfaceStyle
         return self
     }
+
     func contentHuggingPriority(_ priority: UILayoutPriority, for axis: NSLayoutConstraint.Axis) -> Self {
-        self.setContentHuggingPriority(priority, for: axis)
+        setContentHuggingPriority(priority, for: axis)
         return self
     }
+
     func contentCompressionResistancePriority(_ priority: UILayoutPriority, for axis: NSLayoutConstraint.Axis) -> Self {
-        self.setContentCompressionResistancePriority(priority, for: axis)
+        setContentCompressionResistancePriority(priority, for: axis)
         return self
     }
 }
 
 open class View: UIView, StackUIView {
-    public func apply(_ closure: (Self) -> ()) -> Self {
+    public func apply(_ closure: (Self) -> Void) -> Self {
         closure(self)
         return self
     }
